fbfae98bfe165b68de92aadaf959d41f9f4e8d7b
Dastin26 <jqb123456789@gmail.com>
2024-03-03 01:05:46 -0600

    testing users

--- testing2.cpp
+++ testing2.cpp
@@ -8,5 +8,6 @@ int main()
     cout << "Testing branch 1" << endl;
     cout << "Testing commit in branch with new username" << endl;
     cout << "Hellow world again" << endl;
+    cout << "Hellow world again x2" << endl;
     return 0;
 }
