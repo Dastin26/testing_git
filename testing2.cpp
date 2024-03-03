abb5a424744eed31bf4224372336bff3cb3d3270
Dastin26 <jqb123456789@gmail.com>
2024-03-03 01:00:08 -0600

    adding new line and compiling

--- testing2.cpp
+++ testing2.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Hello world!" << endl;
     cout << "Testing branch 1" << endl;
     cout << "Testing commit in branch with new username" << endl;
+    cout << "Hellow world again" << endl;
     return 0;
 }
